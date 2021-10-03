fd3a43a6e14ecd01f010b73ce64e0ba19022f062
JuliKM <57647619+JuliKM@users.noreply.github.com>
2021-10-03 15:59:39 -0300

    Add files via upload

---
+++ TP6/Punto2.js
@@ -0,0 +1,9 @@
+function mostrarMeses(){
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"]
+
+
+for(i=0; i<12; i++){
+    document.write(meses[i])
+}
+
+}
